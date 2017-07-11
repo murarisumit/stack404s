86a9e5cc2d8685ae2ddb2d871e2736f04435c528
Sumit Murari <sumit.murari@fireeye.com>
2017-07-12 01:04:57 +0530

    Added question title to list bar in UI

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -26,22 +26,23 @@ Vue.component('vue-login-component', {
           channelUrl: 'http://localhost/blank',
           // Called when all initialization is finished
           complete: function(data) {
-              $('#login-button')
-                  .removeAttr('disabled')
-                  .text('Click here to login');
-
+            console.log("init done")
           }
         })
         SE.authenticate({
           success: function(data) {
             sessionStorage.setItem("at", data.accessToken);
-            for (s_network of data.networkUsers) {
-              if( s_network.site_url === "https://stackoverflow.com" ) {
+            for (network of data.networkUsers) {
+              //network is each stackoverflow site
+              if( network.site_url === "https://stackoverflow.com" ) {
                 console.log("Got data from SO")
-                sessionStorage.setItem("uid", s_network.user_id);
+                sessionStorage.setItem("uid", network.user_id);
                 login.hide_modal_box()
                 break
               }
+            }
+            if( sessionStorage.getItem('uid') === null) {
+              alert("Unable to fetch USER_ID, something wrong happened")
             }
           },
           error: function(data) {
@@ -93,22 +94,45 @@ var app = new Vue({
   },
   methods: {
     add_working: function(item_obj) {
+      console.log("In: add_working")
       ques_url = this.site_url + "/questions/" + item_obj.question_id
       item_obj["q_url"] = ques_url
       this.answers_working.push({ obj: item_obj})
     },
-    add_404: function(item_obj) {
-      ques_url = this.site_url + "/questions/" + item_obj.question_id
-      item_obj["q_url"] = ques_url
-      this.answers_404.push({ obj: item_obj})
-    },
-    check_if_has404: function(item) {
-      let full_list = '';
+    add_404: function(answer_obj) {
       var self = this
+      console.log("In: add_404")
+      q_url = this.site_url + "/questions/" + answer_obj.question_id
+      answer_obj["ques_url"] = q_url
+
+      fetch_ques_url = this.api_url + "/questions/" + answer_obj.question_id + "?access_token=" + sessionStorage.getItem('at') + "&site=stackoverflow&key=" + this.key
       var settings = {
         "async": true,
         "crossDomain": true,
-        "url": this.api_url + "/answers/" + item.answer_id + "?access_token=" + sessionStorage.getItem('at') + "&site=stackoverflow&key=" + this.key + "&filter=withbody",
+        "url": fetch_ques_url,
+        "method": "GET",
+      }
+      $.ajax(settings).done(function (response) {
+        console.log(response)
+        var q_title = response.items[0]['title']
+        console.log("q_title is: " + q_title)
+        answer_obj['ques_title'] = q_title
+      }).fail(function(msg) {
+        console.log("Unable to pull question title. Error: " + msg)
+        answer_obj['ques_title'] = "Unable to pull question title"
+      }).always(function(msg) {
+        self.answers_404.push({ obj: answer_obj})
+      });
+    },
+    check_if_has404: function(item) {
+      console.log("In: check_if_has404")
+      let full_list = '';
+      var self = this
+      fetch_answer_url = this.api_url + "/answers/" + item.answer_id + "?access_token=" + sessionStorage.getItem('at') + "&site=stackoverflow&key=" + this.key + "&filter=withbody"
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": fetch_answer_url,
         "method": "GET",
       }
       $.ajax(settings).done(function (response) {
@@ -132,21 +156,19 @@ var app = new Vue({
     },
     process_answers: function(items) {
       console.log("In : process_answers()")
-      let full_list = '';
       for (item of items) {
           this.check_if_has404(item)
       }
     },
-    test() {
-      console.log("testing you")
-    },
     get_all_answers: function() {
       console.log("In : get_all_answers()")
       var self = this;
+      var get_token_url =  this.api_url + "/me/answers?access_token=" + sessionStorage.getItem('at') + "&site=" + this.site_name + "&key=" + this.key
+      console.log(get_token_url)
       var settings = {
         "async": true,
         "crossDomain": true,
-        "url": this.api_url + "/me/answers?access_token=" + sessionStorage.getItem('at') + "&site=" + this.site_name + "&key=" + this.key,
+        "url": get_token_url,
         "method": "GET",
       }
       $.ajax(settings).done(function (response) {
